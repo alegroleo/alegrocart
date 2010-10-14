d6157fb490d489c9a029208907eca29dfaf04b65
bthirsk <bthirsk@alegrocart.com>
2010-10-14 10:19:35 +0000

    Modify Country status change to exclude default country

--- AlegroCart_1.2.3/upload/admin/model/environment/model_admin_country.php
+++ AlegroCart_1.2.3/upload/admin/model/environment/model_admin_country.php
@@ -16,14 +16,14 @@ class Model_Admin_Country extends Model {
 		$this->database->query($this->database->parse($sql, $this->request->gethtml('name', 'post'), $this->request->gethtml('country_status', 'post'), $this->request->gethtml('iso_code_2', 'post'), $this->request->gethtml('iso_code_3', 'post'), $this->request->gethtml('address_format', 'post'), $this->request->gethtml('country_id')));
 	}
 	function set_status($status){
-		$this->database->query("update country set country_status = '" . $status . "'");
+		$this->database->query("update country set country_status = '" . $status . "' where country_id != '" . $this->config->get('config_country_id')  ."'");
 	}
 	function delete_country(){
 		$this->database->query("delete from country where country_id = '" . (int)$this->request->gethtml('country_id') . "'");
 	}
 	function check_status(){
-		$result = $this->database->getRows("select country_status from country where country_status = '1'");
-		return $result;
+		$result = count($this->database->getRows("select country_status from country where country_status = '1'"));
+		return $result>1 ? TRUE : FALSE;
 	}
 	function get_country_info(){
 		$result = $this->database->getRow("select distinct * from country where country_id = '" . (int)$this->request->gethtml('country_id') . "'");

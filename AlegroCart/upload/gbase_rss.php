07567cc3552b98e17af91ef4e00f5e2d66026bd6
bthirsk <bthirsk@alegrocart.com>
2010-12-19 11:07:33 +0000

    Add Google RSS and update cart RSS

--- AlegroCart/upload/gbase_rss.php
+++ AlegroCart/upload/gbase_rss.php
@@ -2,6 +2,7 @@
 
 // Installed?
 if (filesize('config.php') == 0) { exit; }
+
 	require_once('library/application/string_modify.php');
 define('APP','CATALOG');
 
@@ -34,6 +35,7 @@ $url =& $locator->get('url'); // URL
 $language =& $locator->get('language'); // Language
 $currency =& $locator->get('currency'); //Currency
 $tax      =& $locator->get('tax'); // Tax
+$weight =& $locator->get('weight');
 $limit = $config->get('config_rss_limit') ? $config->get('config_rss_limit') : 20;
 
 // Base URL
@@ -45,36 +47,46 @@ $product_data = array();
 $sql="select *, p.date_added as date_product_added from product p left join product_description pd on (p.product_id = pd.product_id) left join image i on (p.image_id = i.image_id) where p.status = '1' and pd.language_id = '%s' and p.date_available < now() and p.status = '1' order by date_product_added desc limit " . $limit;
 $sql=sprintf($sql,(int)$language->getId());
 $results = $database->getRows($sql);
-$unit = 'Pounds';
+
 $products=array();
 foreach ($results as $result) {
+	$manufacturer = $database->getRow("select * from manufacturer where manufacturer_id = '" . (int)$result['manufacturer_id'] . "'");
 	$products[]=array(
 	'name' => strip_tags($result['name']),
 	'url' => $url->href('product', FALSE, array('product_id' => $result['product_id'])),
 	'add_date' => date("D, d M Y H:i:s T", strtotime($result['date_product_added'])),
-	'desc' => htmlentities(strip_tags(strippedstring($result['description'],256),'ENT_QUOTES')).htmlentities('<br>'),
+	'desc' => htmlentities(strip_tags(strippedstring($result['description'],256),'ENT_QUOTES')),
+	'thumb' => $image->resize($result['filename'], 200, 200),
+	'id' => $result['product_id'],
 	'price' => $currency->format($tax->calculate($result['price'], $result['tax_class_id'], $config->get('config_tax'))),
-	'image' => htmlentities('<br><img width="100" height="100" src="' . $image->resize($result['filename'], 100, 100) . '">'),
-	'weight' => $result['weight'] . ' ' . $unit
+	'model' => htmlentities($result['model']),
+	'manufacturer' => $manufacturer['name'],
+	'weight' => $weight->format($weight->convert($result['weight'],$result['weight_class_id'], $config->get('config_weight_class_id')),$config->get('config_weight_class_id'))
 	);
 }
 
 header('Content-type: text/xml');
+echo '<?xml version="1.0"?>';
 ?>
-<rss version="2.0">
+<rss version="2.0"
+xmlns:g="http://base.google.com/ns/1.0">
 <channel>
 	<title><?php echo $config->get('config_store'); ?></title>
 	<description><?php echo $config->get('config_store'); ?></description>
 	<link><?php echo $catalog_url; ?></link>
-	<copyright><?php echo $config->get('config_store'); ?></copyright>
+
 <?php foreach ($products as $product) { ?>
 	<item>
         <title><?php echo $product['name']; ?></title>
-		<pubDate><?php echo $product['add_date']; ?></pubDate>
-        <description><?php echo $product['desc'].$product['price']." ".$config->get('config_currency').$product['image']; ?></description>
+		<g:brand><?php echo $product['manufacturer']; ?></g:brand>
+		<g:condition>new</g:condition>
+        <description><?php echo $product['desc']; ?></description>
         <link><?php echo $product['url']; ?></link>
-
-        <guid isPermaLink="true"><?php echo $product['url']; ?></guid>
+        <g:id><?php echo $product['id']; ?></g:id>
+        <g:image_link><?php echo $product['thumb']; ?></g:image_link>
+		<g:price><?php echo $product['price']; ?></g:price>
+		<g:poduct_type><?php echo $product['model']; ?></g:poduct_type>
+		<g:currency><?php echo $config->get('config_currency'); ?></g:currency>
 	</item>
 <?php } ?>
 </channel>

5f613b35eafb127aac1333b3b2c84aef7a0e8991
leo <leo@alegrocart.com>
2011-03-04 22:55:22 +0000

    Modify Checkout Shipping to display non shippable products

--- AlegroCart/upload/catalog/language/english/controller/checkout_shipping.php
+++ AlegroCart/upload/catalog/language/english/controller/checkout_shipping.php
@@ -9,6 +9,12 @@ $_['text_shipping_address'] = 'Shipping Address';
 $_['text_shipping_method']  = 'Shipping Method';
 $_['text_shipping_methods'] = 'Please select the preferred shipping methods to use on this order.';
 $_['text_comments']         = 'Add Comments About Your Order';
+$_['text_nonshippable']     = 'Please note that the following products are non shippable. You have to pick up them in our warehouse.';
+$_['text_choose']           = 'Please choose shipping address and shipping method below for the others.';
+$_['text_image']            = 'Image';
+$_['text_name']             = 'Name';
+$_['text_quantity']         = 'Quantity';
+$_['text_ship']             = 'Ship';
 
 // Error
 $_['error_shipping']        = 'Error: Shipping method required!';

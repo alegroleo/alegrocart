13048d153138a2329c210155d4d18d561626750c
bthirsk@alegrocart.com <bthirsk@alegrocart.com@258b8abd-d880-28c3-ba27-2c39e9e57e77>
2011-04-06 11:15:05 +0000

    Modify maintenance for selectable logos

--- trunk/AlegroCart/upload/catalog/extension/module/header.php
+++ trunk/AlegroCart/upload/catalog/extension/module/header.php
@@ -22,20 +22,20 @@ class ModuleHeader extends Controller {
 					$view->set('rss_link', $url->get_server().'rss.php');
 				}
 				
-				$view->set('store_logo', $image->href('logo/'.$config->get('config_store_logo')));
-				$view->set('logo_left', $config->get('config_logo_left'));
-				$view->set('logo_top', $config->get('config_logo_top'));
-				$view->set('logo_width', $config->get('config_logo_width'));
-				$view->set('logo_height', $config->get('config_logo_height'));
-				$view->set('store', $config->get('config_store'));
-				
 				$home_data = $this->modelCore->get_homepage();
 				$template->set('meta_title', $home_data['meta_title'] ? $home_data['meta_title'] : FALSE);
 				$template->set('meta_description', $home_data['meta_description'] ?'<meta name="description" content="' . $home_data['meta_description'] . '">' : FALSE);
 				$template->set('meta_keywords', $home_data['meta_keywords'] ? '<meta name="keywords" content="'.$home_data['meta_keywords'] . '">' : FALSE);
 			} else {
 			$session->set('maintenance', '');
+			
 			}
+			$view->set('store_logo', $image->href('logo/'.$config->get('config_store_logo')));
+			$view->set('logo_left', $config->get('config_logo_left'));
+			$view->set('logo_top', $config->get('config_logo_top'));
+			$view->set('logo_width', $config->get('config_logo_width'));
+			$view->set('logo_height', $config->get('config_logo_height'));
+			$view->set('store', $config->get('config_store'));
     		return $view->fetch('module/header.tpl');
 		}
 	}

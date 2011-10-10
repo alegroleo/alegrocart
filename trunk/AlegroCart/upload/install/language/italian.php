c55a6bab3a4277abeffec786177d23b2f102895f
bthirsk@alegrocart.com <bthirsk@alegrocart.com@258b8abd-d880-28c3-ba27-2c39e9e57e77>
2011-10-10 23:56:31 +0000

    Add Italian to installer

---
+++ trunk/AlegroCart/upload/install/language/italian.php
@@ -0,0 +1,52 @@
+<?php
+// Heading
+$_['heading_title']    = 'Installazione AlegroCart ';
+$_['heading_step1']    = 'Passo 1/3 - Dettagli Database';
+$_['heading_step2']    = 'Passo 2/3 - Amministrazione';
+$_['heading_step3']    = 'Terminato!';
+
+// Error
+$_['error_not_found']  = '%s non e\'stato trovato! (sicuro di averlo caricato)';
+$_['error_not_777']    = '%s non e\' scrivibile ! (chmod a+w o chmod 777)';
+$_['error_not_666']    = '%s non e\' scrivibile ! (chmod a+w or chmod 666)';
+$_['error_php']        = 'E\'richiesto PHP 5.0 o superiore per AlegroCart';
+$_['error_session']    = 'Devi disabilitare session.auto_start in php.ini per usare AlegroCart';
+$_['error_mysql']      = 'L\'estensione MySql e\'richiesta per far funzionare AlegroCart';
+$_['error_gd']         = 'L\'estensione GD e\' richiesta per far funzionare AlegroCart';
+$_['error_upload']     = 'File caricati sono richiesti per abilitare AlegroCart';
+$_['error_zlib']       = 'ZLIB deve essere cercato in php.ini per far funzionare AlegroCart ';
+$_['error']            = 'I seguenti errori sono apparsi';
+$_['error_fix']        = 'Per favore correggi gli errori precedenti, installlazione fermata';
+$_['error_dbhost']     = 'L\'Host Database e\'richiesto';
+$_['error_dbuser']     = 'Lo Username del Database e\'richiesto';
+$_['error_dbname']     = 'Il nome del Database e\'richiesto';
+$_['error_dbconnect']  = 'Non potresti connetterti al server database usando lo username and password provvisoria.';
+$_['error_dbperm']     = 'Il database potrebbe non essere accessibile. Controlla che tu hai i permessi e che esista sul server.';
+$_['error_sql']        = 'Il file Install SQL %s potrebbe non essere stato trovato.';
+$_['error_admin_uname']= 'Lo username Admin e\' richiesto';
+$_['error_admin_passw']= 'La password Admin e\' richiesta';
+$_['error_write']      = 'Potresti non scrivere nel file %s .';
+$_['error_open']       = 'Potrebbe non essere aperto il file %s per essere scritto.';
+
+// Text
+$_['ac']               = 'AlegroCart Home';
+$_['acforum']          = 'Supporto AlegroCart';
+$_['fresh']            = 'QUESTI E\' PER SOLO PER UNA NUOVA INSTALLAZIONE ! IL TUO ARCHIVIO SARA\' RIMOSSO!';
+$_['database_details'] = 'Per favore immetti i dettagli di connessione del tuo Archivio.';
+$_['dbhost']           = 'Host Database:';
+$_['dbuser']           = 'Username Database :';
+$_['dbpassw']          = 'Password Database :';
+$_['dbname']           = 'Nome Database :';
+$_['continue']         = 'Continua';
+$_['admin_details']    = 'Per favore immetti un username e password per la amministrazione.';
+$_['uname']            = 'Username:';
+$_['passw']            = 'Password:';
+$_['success']          = '%s e\'stato aggiornato con successo.';
+$_['del_inst']         = 'Tu DEVI cancellare questa cartella di installazione!';
+$_['config']           = 'Modifica \'config.php\' non scrivibile (chmod go-w or chmod 644).';
+$_['htaccess']         = 'Modifica \'.htaccess\' non scrivibile (chmod go-w 644).';
+$_['congrat']          = 'Congratulazioni! Hai installato AlegroCart con successo.';
+$_['congrat_upg']      = 'Congratulazioni ! Hai aggiornato AlegroCart con successo.';
+$_['shop']             = 'Negozio Online';
+$_['admin']            = 'Amministrazione';
+?>

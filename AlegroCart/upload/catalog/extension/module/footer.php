66aacf59f78fbb6b6a0976468b4cb766e21acbaf
bthirsk <bthirsk@alegrocart.com>
2011-10-16 16:44:09 +0000

    Add Date Class

--- AlegroCart/upload/catalog/extension/module/footer.php
+++ AlegroCart/upload/catalog/extension/module/footer.php
@@ -6,6 +6,7 @@ class ModuleFooter extends Controller {
 		$language =& $this->locator->get('language');	
 		$session  =& $this->locator->get('session');
 		$request  =& $this->locator->get('request');
+		$dateformat =& $this->locator->get('dates');
 		$this->modelCore 	= $this->model->get('model_core');
 		
 		if ($config->get('footer_status')) {	
@@ -19,21 +20,31 @@ class ModuleFooter extends Controller {
 			$view->set('footer_logo_top', $config->get('footer_logo_top'));
 			$view->set('footer_logo_width', $config->get('footer_logo_width'));
 			$view->set('footer_logo_height', $config->get('footer_logo_height'));
-			
+			$view->set('todays_date', $dateformat->getDate($language->get('date_format_long')));
 			$view->set('store', $config->get('config_store'));
 			$view->set('show_version',$config->get('show_version'));
 			$view->set('version', $config->get('version'));
 			$view->set('text_version', $language->get('text_version'));
+			
+			$view->set('text_footer_logo', $language->get('text_footer_logo'));
+			$view->set('text_papal', $language->get('text_papal'));
+			$view->set('text_visa', $language->get('text_visa'));
+			$view->set('text_mastercard', $language->get('text_mastercard'));
+			$view->set('text_wc3html', $language->get('text_wc3html'));
+			$view->set('text_wc3css', $language->get('text_wc3css'));
+			
 			$view->set('text_powered_by', $language->get('text_powered_by',$config->get('config_store'),date('Y')));
 
 			return $view->fetch('module/footer.tpl');
 		}
 	}
+
 	function currentpage($current_page){  
 		switch($current_page){
 			case '':
 			case 'home':
 			case 'information':
+			case 'manufacturer':
 			case 'sitemap':
 			case 'search':
 			case 'contact':
--- AlegroCart/upload/catalog/language/english/extension/module/footer.php
+++ AlegroCart/upload/catalog/language/english/extension/module/footer.php
@@ -1,12 +1,18 @@
 <?php
 // Text
-$_['text_home']       = 'Home';
-$_['text_sitemap']    = 'Site Map';
-$_['text_account']    = 'Account';
-$_['text_login']      = 'Log In';
-$_['text_logout']     = 'Log Off';
-$_['text_cart']       = 'Cart Content';
-$_['text_checkout']   = 'Checkout';
-$_['text_version']    = ' Version - ';
-$_['text_powered_by'] = 'Powered by <a href="http://www.alegrocart.com/">AlegroCart</a><br> %s &copy; %s';
+$_['text_home']       	= 'Home';
+$_['text_sitemap']    	= 'Site Map';
+$_['text_account']    	= 'Account';
+$_['text_login']      	= 'Log In';
+$_['text_logout']     	= 'Log Off';
+$_['text_cart']       	= 'Cart Content';
+$_['text_checkout']   	= 'Checkout';
+$_['text_footer_logo']	= 'SSL';
+$_['text_papal']		= 'PayPal';
+$_['text_visa']			= 'Visa';
+$_['text_mastercard']	= 'Master Card';
+$_['text_wc3html']		= 'WC3 HTML';
+$_['text_wc3css']		= 'WC3 CSS';
+$_['text_version']    	= ' Version - ';
+$_['text_powered_by'] 	= 'Powered by <a href="http://www.alegrocart.com/">AlegroCart</a><br> %s &copy; %s';
 ?>

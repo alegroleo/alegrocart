efab26f56a2c396d3c5125383cb68ecb9b006f2d
leo@alegrocart.com <leo@alegrocart.com@258b8abd-d880-28c3-ba27-2c39e9e57e77>
2011-10-26 11:38:42 +0000

    Add Catalan to Installer

---
+++ trunk/AlegroCart/upload/install/language/catalan.php
@@ -0,0 +1,52 @@
+<?php
+// Heading
+$_['heading_title']    = 'AlegroCart Instal.laci&#243;';
+$_['heading_step1']    = 'Pas 1 / 3 - Detalls de la base de dades';
+$_['heading_step2']    = 'Pas 2 / 3 - Administraci&#243;';
+$_['heading_step3']    = 'Acabat!';
+
+// Error
+$_['error_not_found']  = '%s NO s&#180;ha trobat!(assegurar-se que t&#233; l&#180;hi ha descarregat)';
+$_['error_not_777']    = '%s NO &#233;s escriptura! (chmod a+w o chmod 777)';
+$_['error_not_666']    = '%s &#161;NO es escribible! (chmod a+w o chmod 666)';
+$_['error_php']        = 'Es requereix PHP 5.0 o superior per AlegroCart';
+$_['error_session']    = 'Deu inhabilitar session.auto_start en php.ini per utilitzar AlegroCart';
+$_['error_mysql']      = 'Extensi&#243; de MySQL &#233;s necessari per executar AlegroCart';
+$_['error_gd']         = 'Extensi&#243; de GD &#233;s necess&#224;ria per executar AlegroCart';
+$_['error_upload']     = 'La c&#224;rrega d&#180;arxius &#233;s necessari per estar en condicions d&#180;executar AlegroCart';
+$_['error_zlib']       = 'ZLIB ha d&#180;estar carregat en php.ini per AlegroCart per treballar';
+$_['error']            = 'Els seg&#252;ents errors:';
+$_['error_fix']        = 'Si us plau, corregeixi el/s error/s anterior/s, instal.laci&#243; detinguda!';
+$_['error_dbhost']     = 'Es requereix base dates Host';
+$_['error_dbuser']     = 'Es requereix Usuari';
+$_['error_dbname']     = 'Es requereix Nom base dates';
+$_['error_dbconnect']  = 'No s&#180;ha pogut connectar amb el servidor de bases de dades utilitzant el nom d&#180;usuari i la contrasenya proporcionada.';
+$_['error_dbperm']     = 'La base de dades no es pot accedir. Comproveu que t&#233; permisos, i que hi ha al servidor.';
+$_['error_sql']        = 'Arxiu d&#180;instal.laci&#243; de SQL %s no s&#180;ha pogut trobar.';
+$_['error_admin_uname']= 'Es requereix Usuari Admin';
+$_['error_admin_passw']= 'Es requereix Contrasenya Admin';
+$_['error_write']      = 'No es va poder escriure en l&#180;arxiu %s';
+$_['error_open']       = 'No es va poder obrir l&#180;arxiu %s per escriptura.';
+
+// Text
+$_['ac']               = 'Inici AlegroCart';
+$_['acforum']          = 'Suport AlegroCart';
+$_['fresh']            = '&#161;AIX&#210; &#201;S NOM&#201;S PER A INSTAL.LACIONS NOVES! LA SEVA BASE DE DADES SER&#192; ELIMINADA!';
+$_['database_details'] = 'Si us plau, introdueixi les seves dades de connexi&#243; de la base de dades.';
+$_['dbhost']           = 'Base de dades de Host:';
+$_['dbuser']           = 'Base de dades d&#180;Usuari:';
+$_['dbpassw']          = 'Base de dades de Contrasenya:';
+$_['dbname']           = 'Nom Base de dades:';
+$_['continue']         = 'Cont&#237;nua';
+$_['admin_details']    = 'Si us plau, introdueixi un nom d&#180;Usuario y una Contrasenya per l&#180;administraci&#243;.';
+$_['uname']            = 'Usuari:';
+$_['passw']            = 'Contrasenya:';
+$_['success']          = '%s s&#180;ha actualitzat correctament.';
+$_['del_inst']         = 'Ha d&#180;ELIMINAR el directori d&#180;instal.laci&#243;!';
+$_['config']           = 'Passar \'config.php\'ha NO escriptura (chmod go-w o chmod 644).';
+$_['htaccess']         = 'Passar \'.htaccess\' ha NO escriptura(chmod go-w 644).';
+$_['congrat']          = '&#161;Felicitats! Vost&#232; ha instal.lat amb &#232;xit AlegroCart.';
+$_['congrat_upg']      = '`&#161;Felicitats! AlegroCart ha millorat.';
+$_['shop']             = 'Botiga Online';
+$_['admin']            = 'Administraci&#243;';
+?>

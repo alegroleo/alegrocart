b87c744457df5d63136291c84c7e9c0cbb862c08
leo@alegrocart.com <leo@alegrocart.com@258b8abd-d880-28c3-ba27-2c39e9e57e77>
2012-04-22 19:41:09 +0000

    Add Romanian to Installer

---
+++ trunk/AlegroCart/upload/install/language/ro.php
@@ -0,0 +1,57 @@
+<?php
+// Heading
+$_['heading_title']    = 'Instalare AlegroCart ';
+$_['heading_step1']    = 'Pasul 1/3 - Detalii despre baza de date';
+$_['heading_step2']    = 'Pasul 2/3 - Administrre';
+$_['heading_step3']    = 'Terminare!';
+
+// Error
+$_['error_not_found']  = '%s nu a fost gasit! (asigurati-va ca l-ati upload-at)';
+$_['error_not_777']    = '%s nu se poate scrie! (chmod a+w or chmod 777)';
+$_['error_not_666']    = '%s nu se poate scrie! (chmod a+w or chmod 666)';
+$_['error_php']        = 'PHP 5.0 sau mai mare e necesar pentru AlegroCart';
+$_['error_session']    = 'Trebuie sa dezactivati session.auto_start in php.ini pentru a folosi AlegroCart';
+$_['error_mysql']      = 'Extensia MySql este necesara pentru a rula AlegroCart';
+$_['error_gd']         = 'Extensia GD este necesara pentru a rula AlegroCart';
+$_['error_upload']     = 'Trebuie sa permiteti File uploads (Upload fisiere) pentru a rula AlegroCart';
+$_['error_zlib']       = 'ZLIB trebuie sa fie incarcat in php.ini pentru ca AlegroCart sa functioneze';
+$_['error']            = 'Au aparut urmatoarele erori:';
+$_['error_fix']        = 'Instalare oprita - va rugam remediati urmatoarele erori!';
+$_['error_dbhost']     = 'Specificati Database Host';
+$_['error_dbuser']     = 'Specificati Database Username';
+$_['error_dbname']     = 'Specificati Database Name ';
+$_['error_dbconnect']  = 'Nu se poate realiza conexiunea la serverul bazei de date folosind numele de utilizator si parola furnizate.';
+$_['error_dbperm']     = 'Baza de date nu poate fi accesata. Verificati daca aveti permisiuni, si daca aceasta exista pe server.';
+$_['error_sql']        = 'Fisierul de instalare SQL  %s nu a putut fi gasit.';
+$_['error_admin_uname']= 'Specificati numele administratorului';
+$_['error_admin_passw']= 'Specificati parola administratorului';
+$_['error_write']      = 'Nu se poate scrie in fisierul %s .';
+$_['error_open']       = 'Nu se poate deschide fisierul %s pentru scriere.';
+
+// Text
+$_['ac']               = 'AlegroCart Acasa';
+$_['acforum']          = 'AlegroCart Suport';
+$_['fresh']            = 'ACEASTA ESTE NUMAI PENTRU PRIMA INSTALARE! BAZA DE DATE VA FI STEARSA!';
+$_['database_details'] = 'Va rugam specificati detaliile de conectare la baza dumneavoastra de date.';
+$_['dbhost']           = 'Database Host:';
+$_['dbuser']           = 'Database Username:';
+$_['dbpassw']          = 'Database Password:';
+$_['dbname']           = 'Database Name:';
+$_['continue']         = 'Continuare';
+$_['admin_details']    = 'Va rugam specificati un nume de utilizator si o parola pentru administrare.';
+$_['uname']            = 'Username:';
+$_['passw']            = 'Password:';
+$_['success']          = '%s a fost actualizat cu succes.';
+$_['del_inst']         = 'TREBUIE sa stergeti folderul install!';
+$_['config']           = 'Faceti \'config.php\' protejat la scriere (chmod go-w or chmod 644).';
+$_['htaccess']         = 'Faceti \'.htaccess\' protejat la scriere (chmod go-w 644).';
+$_['congrat']          = 'Felicitari! Ati instalat cu succes AlegroCart.';
+$_['congrat_upg']      = 'Felicitari! Ati actualizat cu succes AlegroCart.';
+$_['shop']             = 'Magazin online';
+$_['admin']            = 'Administrare';
+$_['method']           = 'Metoda de instalare:';
+$_['default_install']  = 'Instalare implicita (cu date de test)';
+$_['clean_install']    = 'Instalare curata (fara date de test)';
+$_['default_expl']     = 'Aceasta va instala produse de test, grupe, producatori, imagini, opinii etc. deasemenea. Acestea sunt pentru a testa AlegroCart';
+$_['clean_expl']       = 'Aceasta va inlatura toate produsele si datele legate de acestea. Aceasta este pentru crearea unei instalari curate, fara produse.';
+?>

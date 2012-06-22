7add37a61319a0db4df4dbedf4e1f8280eadc161
leo <leo@alegrocart.com>
2012-06-22 11:29:32 +0000

    Add Indonesian to Installer

---
+++ AlegroCart/upload/install/language/id.php
@@ -0,0 +1,57 @@
+<?php
+// Heading
+$_['heading_title']    = 'Pemasangan AlegroCart';
+$_['heading_step1']    = 'Langkah 1/3 - Rincian Database';
+$_['heading_step2']    = 'Langkah 2/3 - Administrasi';
+$_['heading_step3']    = 'Selesai!';
+
+// Error
+$_['error_not_found']  = '%s tidak ditemukan! (pastikan anda telah mengunggahnya)';
+$_['error_not_777']    = '%s tidak bisa tersimpan! (chmod a+w atau chmod 777)';
+$_['error_not_666']    = '%s tidak bisa tersimpan! (chmod a+w atau chmod 666)';
+$_['error_php']        = 'PHP 5.0 atau lebih tinggi diperlukan bagi AlegroCart';
+$_['error_session']    = 'Anda harus mematikan session.auto_start di php.ini untuk menggunakan AlegroCart';
+$_['error_mysql']      = 'Extensi MySql diperlukan untuk menjalankan AlegroCart';
+$_['error_gd']         = 'Extensi GD diperlukan untuk menjalankan AlegroCart';
+$_['error_upload']     = 'File uploads perlu dihidupkan untuk menjalankan AlegroCart';
+$_['error_zlib']       = 'ZLIB harus termuat di php.ini agar AlegroCart dapat bekerja';
+$_['error']            = 'Kesalahan berikut terjadi:';
+$_['error_fix']        = 'Silakan perbaiki kesalahan diatas, pemasangan dihentikan!';
+$_['error_dbhost']     = 'Host Database diperlukan';
+$_['error_dbuser']     = 'Username Database diperlukan';
+$_['error_dbname']     = 'Nama Database diperlukan';
+$_['error_dbconnect']  = 'Tidak dapat terhubung ke server database menggunakan username dan password yang diberikan.';
+$_['error_dbperm']     = 'Database tidak dapat diakses. Periksa apakah Anda memiliki hak akses, dan tersedia di server.';
+$_['error_sql']        = 'Berkas Installasi SQL %s tidak dapat ditemukan.';
+$_['error_admin_uname']= 'Username admin diperlukan';
+$_['error_admin_passw']= 'Password admin diperlukan';
+$_['error_write']      = 'Tidak dapat menulis ke berkas %s.';
+$_['error_open']       = 'Tidak dapat membuka berkas %s untuk ditulis.';
+
+// Text
+$_['ac']               = 'AlegroCart Depan';
+$_['acforum']          = 'Bantuan AlegroCart';
+$_['fresh']            = 'INI HANYA UNTUK PEMASANGAN BARU! DATABASE ANDA AKAN DIHAPUS!';
+$_['database_details'] = 'Silakan masukkan informasi database anda.';
+$_['dbhost']           = 'Host Database:';
+$_['dbuser']           = 'Username Database:';
+$_['dbpassw']          = 'Database Password:';
+$_['dbname']           = 'Nama Database:';
+$_['continue']         = 'Lanjutkan';
+$_['admin_details']    = 'Silakan masukkan username dan password untuk administrasi';
+$_['uname']            = 'Nama pengguna:';
+$_['passw']            = 'Sandi:';
+$_['success']          = '%s telah berhasil diperbarui.';
+$_['del_inst']         = 'Anda HARUS menghapus direktori installasi ini!';
+$_['config']           = 'Buatlah \'config.php\' tidak dapat diubah (chmod go-w atau chmod 644).';
+$_['htaccess']         = 'Buatlah \'.htaccess\' tidak dapat diubah (chmod go-w atau chmod 644).';
+$_['congrat']          = 'Selamat! Anda telah berhasil memasang AlegroCart.';
+$_['congrat_upg']      = 'Selamat! Anda telah berhasil memperbarui AlegroCart.';
+$_['shop']             = 'Toko Online';
+$_['admin']            = 'Administrasi';
+$_['method']           = 'Metode Installasi:';
+$_['default_install']  = 'Installasi Standar';
+$_['clean_install']    = 'Installasi \'Bersih\'';
+$_['default_expl']     = 'Ini juga akan memasang contoh produk, kategori, pabrikan, gambar, tinjauan dll. Ini untuk menguji AlegroCart';
+$_['clean_expl']       = 'Ini kan menghapus setiap produk dan data yang berkaitan. Ini untuk membuat installasi bersih tanpa produk.';
+?>
